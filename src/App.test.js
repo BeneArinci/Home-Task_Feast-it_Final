72ca396f040b444131793e24c1cff9982d081a02
Bene Arinci <benedetta.arinci@gmail.com>
2020-09-22 16:29:45 +0100

    Added test for the action of filtering by drinks

--- src/App.test.js
+++ src/App.test.js
@@ -2,18 +2,39 @@ import React from 'react'
 import { screen, render, waitForElement } from "@testing-library/react";
 import App from './App'
 import mockBasicResponse from '../__mocks__/api-response.json'
+import mockDrinkResponse from '../__mocks__/drink-api-response.json'
 
-jest.spyOn(global, "fetch").mockImplementation(() => {
-  return Promise.resolve({
-    json: () => Promise.resolve(mockBasicResponse)
+
+
+test ('shows suppliers in the main page', async () =>{
+  
+  jest.spyOn(global, "fetch").mockImplementation(() => {
+    return Promise.resolve({
+      json: () => Promise.resolve(mockBasicResponse)
+    })
   })
-})
 
-test ('shows providers in the main page', async () =>{
   render(<App />)
 
   const element = await waitForElement(() => screen.getByText("I AM A TEST PROVIDER"))
   screen.debug()
   expect(element).toBeInTheDocument()
 })
+
+test ('shows drink suppliers when "drink" selection is made', async () =>{
+  jest.spyOn(global, "fetch").mockImplementation(() => {
+    return Promise.resolve({
+      json: () => Promise.resolve(mockDrinkResponse)
+    })
+  })
+
+  render(<App />)
+  const drinkBtn = screen.getByRole("button", {name: /Drinks/})
+  drinkBtn.click()
+  const element = await waitForElement(() => screen.getByText("I AM A MOCK DRINKS SUPPLIER"))
+  screen.debug()
+  expect(element).toBeInTheDocument()
+})
+
+
 
