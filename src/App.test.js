86f03e48899a957313665fd5525f8584569faf53
Bene Arinci <benedetta.arinci@gmail.com>
2020-09-22 16:36:38 +0100

    Added test for the action of filtering by florists

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import { screen, render, waitForElement } from "@testing-library/react";
 import App from './App'
 import mockBasicResponse from '../__mocks__/api-response.json'
 import mockDrinkResponse from '../__mocks__/drink-api-response.json'
+import mockFloristsResponse from '../__mocks__/florists-api-response.json'
+
 
 
 
@@ -32,6 +34,36 @@ test ('shows drink suppliers when "drink" selection is made', async () =>{
   const drinkBtn = screen.getByRole("button", {name: /Drinks/})
   drinkBtn.click()
   const element = await waitForElement(() => screen.getByText("I AM A MOCK DRINKS SUPPLIER"))
+  expect(element).toBeInTheDocument()
+})
+
+
+test ('shows drink suppliers when "drink" selection is made', async () =>{
+  jest.spyOn(global, "fetch").mockImplementation(() => {
+    return Promise.resolve({
+      json: () => Promise.resolve(mockDrinkResponse)
+    })
+  })
+
+  render(<App />)
+  const drinkBtn = screen.getByRole("button", {name: /Drinks/})
+  drinkBtn.click()
+  const element = await waitForElement(() => screen.getByText("I AM A MOCK DRINKS SUPPLIER"))
+  expect(element).toBeInTheDocument()
+})
+
+
+test ('shows florists suppliers when "florists" selection is made', async () =>{
+  jest.spyOn(global, "fetch").mockImplementation(() => {
+    return Promise.resolve({
+      json: () => Promise.resolve(mockFloristsResponse)
+    })
+  })
+
+  render(<App />)
+  const floristsBtn = screen.getByRole("button", {name: /Florists/})
+  floristsBtn.click()
+  const element = await waitForElement(() => screen.getByText("AMORE FLOREALS MOCK"))
   screen.debug()
   expect(element).toBeInTheDocument()
 })
