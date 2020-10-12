a70993f972686a05e1d02dc5b3257516b6b7b2ec
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-12 23:23:21 +0100

    Added filtering options

---
+++ src/filteringOptions.js
@@ -0,0 +1,26 @@
+export const filteringOptions = [
+  {
+    name: "Food",
+    filter: "food"
+  },
+  {
+    name: "Drink",
+    filter: "drink"
+  },
+  {
+    name: "Staff",
+    filter: "staff"
+  },
+  {
+    name: "Florists",
+    filter: "florists"
+  },
+  {
+    name: "Marquees-Tents",
+    filter: "marquees_tents"
+  },
+  {
+    name: "Audio-Visual",
+    filter: "audio_visual"
+  }
+];
